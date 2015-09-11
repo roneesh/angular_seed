a3cb51e0ee5d99cfa0998a393d2088304ae4ec1b
Rohit Varicatt <rvaric0@searshc.com>
2015-09-11 15:18:38 -0500

    Adding dist to repo for Sushma - TODO remove later

--- app/js/author.js
+++ app/js/author.js
@@ -7,7 +7,7 @@ page.factory('pageCtx', function(){
             return JSON.parse('{"1": 1, "2": 2, "3": {"4": 4, "5": {"6": 6}}}');
         },
         cookieType: function(){
-            return "cookieType";
+            return "cookieType565656";
         },
         campaignName: function(){
             return "campaignName";
