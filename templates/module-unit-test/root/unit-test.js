02e643130c7b5b91553440e8e3762af9f4815fbc
Roneesh Vashisht <rvashis@searshc.com>
2016-03-04 12:10:50 -0600

    update to unit-testing template

--- templates/module-unit-test/root/unit-test.js
+++ templates/module-unit-test/root/unit-test.js
@@ -1,15 +1,18 @@
-describe("{%= app_name %} application", function() {
+describe("{%= module_name %}Controller", function() {
 
     var scope;
 
     beforeEach(module('{%= app_name %}'));
 
     // Loads the controllers
-    beforeEach(inject(function($controller, $rootScope) {
+    beforeEach(inject(function($controller, $rootScope, $location, $scope, $state, {%= module_name %}Factory) {
+
         scope = $rootScope.$new();
+
         $controller('{%= module_name %}Controller', {
             $scope: scope
         });
+
     }));
 
     it("should have a scope and scope.ui variable initialized", inject(function() {
