26f0b62c67d15f8671705101222c03f4244a2b4b
Roneesh Vashisht <rvashis@searshc.com>
2016-03-09 15:56:11 -0600

    removing asapBoilerplate folder

--- app/asapBoilerplate/author.js
+++
@@ -1,9 +0,0 @@
-var page = angular.module('page', []);
-page.factory('pageCtx', function($q){
-    return {
-        someJSPValue: function(){
-            // value given to JS by the CQ5 dialog
-            return "${DIALOGUEVALUE}";
-        }
-    };
-});
