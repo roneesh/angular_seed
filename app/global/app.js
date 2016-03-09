ea421b1c2aafdddf4c861cf032d526c75b10b781
Roneesh Vashisht <rvashis@searshc.com>
2016-03-09 16:57:52 -0600

    adding in new views folder structure and putting it in as example in app.js

--- app/global/app.js
+++ app/global/app.js
@@ -8,7 +8,7 @@
     function config($stateProvider, $locationProvider) {
         // $stateProvider
         //     .state('home', {
-        //         url: '/',
+        //         url: '/etc/designs/shc/change_this_to_your_component_name/partials/example.html',
         //         templateUrl: '',
         //         controller: '',
         //         controllerAs: 'vm'
