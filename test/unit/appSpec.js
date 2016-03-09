fde82e6c9c196524946313589cadbd23b90df08b
Roneesh Vashisht <rvashis@searshc.com>
2016-03-09 15:57:48 -0600

    basic test commented out

--- test/unit/appSpec.js
+++ test/unit/appSpec.js
@@ -1,29 +1,29 @@
 describe("ngcq5 application", function() {
 
-    var scope;
-    var pageCtx = {
-        configJson: function() {
-            return JSON.parse('{"1": 1, "2": 2, "3": {"4": 4, "5": {"6": 6}}}');
-        },
-        cookieType: function() {
-            return "cookieType";
-        },
-        campaignName: function() {
-            return "campVal";
-        },
-    };
+    // var scope;
+    // var pageCtx = {
+    //     configJson: function() {
+    //         return JSON.parse('{"1": 1, "2": 2, "3": {"4": 4, "5": {"6": 6}}}');
+    //     },
+    //     cookieType: function() {
+    //         return "cookieType";
+    //     },
+    //     campaignName: function() {
+    //         return "campVal";
+    //     },
+    // };
 
 
-    beforeEach(module('ngcq5'));
+    // beforeEach(module('ngcq5'));
 
     // Loads the controllers
-    beforeEach(inject(function($controller, $rootScope) {
-        scope = $rootScope.$new();
-        $controller('mainController', {
-            $scope: scope,
-            pageCtx: pageCtx
-        });
-    }));
+    // beforeEach(inject(function($controller, $rootScope) {
+    //     scope = $rootScope.$new();
+    //     $controller('mainController', {
+    //         $scope: scope,
+    //         pageCtx: pageCtx
+    //     });
+    // }));
 
 
     // it("should have a success message initialized", inject(function() {
